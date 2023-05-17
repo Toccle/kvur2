d0bedcf1c50962702c2888620a8a28c8c2382c6a
Toccle <131477475+Toccle@users.noreply.github.com>
2023-05-17 18:44:37 +0300

    Исправил ошибку в 19 строке и добавил обработку ошибок

--- kvtest.py
+++ kvtest.py
@@ -7,7 +7,7 @@ while baddata:
         b = int(input('Введите b: '))
         c = int(input('Введите с: '))
         baddata = False
-    except:
+    except ValueError:
         print('Не удалось получить данные!')
 
 D = (b * b) - (4 * a * c)
@@ -16,7 +16,7 @@ if D > 0:
     d = sqrt(D)
     X1 = ((-b) + d) / (2 * a)
     X2 = ((-b) - d) / (2 * a)
-    print(f'Уравнение имеет два корня X1 = (X1}, X2 = {X2}.')
+    print(f'Уравнение имеет два корня X1 = {X1}, X2 = {X2}.')
 
 elif D == 0:
     X1 = (-b) / (2 * a)
